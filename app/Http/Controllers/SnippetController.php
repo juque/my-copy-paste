3ebed582142e6fe2b5427090f54b50ac87b21bce
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-10-16 16:41:30 -0300

    create migration for Snippet

---
+++ app/Http/Controllers/SnippetController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Snippet;
+use Illuminate\Http\Request;
+
+class SnippetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Snippet $snippet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Snippet $snippet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Snippet $snippet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Snippet $snippet)
+    {
+        //
+    }
+}
