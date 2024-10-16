d3838d5a0c82fa67ce48e964347b06dd5bcd10b3
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-10-16 16:46:48 -0300

    create layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <title>My Copy Paste</title>
+  
+</head>
+
+<body>
+
+  {{ $slot }}
+  
+</body>
+</html>
