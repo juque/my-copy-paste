29d5d5bb031af1390711eeb6c5289ae198db5805
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-10-19 18:36:19 -0300

    add Spatie tags package

--- app/Models/Snippet.php
+++ app/Models/Snippet.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
 
 class Snippet extends Model
 {
+  use HasTags;
+
   protected $fillable = ['title', 'content', 'note'];
+
 }
