e4a534bc57e7b30420a7d8a8085a3a134706666d
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-10-19 02:20:59 -0300

    add native copy paste

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,22 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
+
+// Native Clipboard
+document.querySelectorAll('.btn-copy').forEach((button, index) => {
+
+	button.addEventListener('click', function () {
+
+		const textToCopy = document.querySelectorAll('.clipboard')[index].textContent;
+
+		navigator.clipboard.writeText(textToCopy)
+			.then(() => {
+				console.log('Text copied to clipboard: ', textToCopy);
+			})
+			.catch(err => {
+				console.error('Error copying the text: ', err);
+			});
+
+	});
+
+});
