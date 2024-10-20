27063b13762e1e73cbcf7357397e336edec7a355
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-10-20 13:31:20 -0300

    refactor: enhance form input component to use dynamic default values
    Updated the input component to allow dynamic default values by replacing
    a hardcoded reference to 'title' with a variable. Now uses `old($name,
    $value)` to provide better flexibility and reusability across different
    form fields.

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -4,7 +4,7 @@
 ])
 
 
-<input {{ $attributes->merge(['class' => 'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5']) }} type="text" name="{{ $name }}" value="{{ old('title')}}" />
+<input {{ $attributes->merge(['class' => 'shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5']) }} type="text" name="{{ $name }}" value="{{ old($name, $value)}}" />
 
 @error($name)
 <div class="text-red-500">
