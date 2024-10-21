8b6a13bf2c1295f012637b35e7753df465d26bd4
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-10-20 22:59:02 -0300

    create a snippet single component

--- resources/views/snippets/by-tag.blade.php
+++ resources/views/snippets/by-tag.blade.php
@@ -1,21 +1,13 @@
 <x-layout>
-<h2>Snippets con el tag "{{ $tag }}"</h2>
+<h2>Snippets etiquetados con "{{ $tag }}"</h2>
 
 @if ($snippets->isEmpty())
     <p>No hay snippets con este tag.</p>
 @else
-    @foreach ($snippets as $snippet)
-        <div class="snippet">
-            <h3>{{ $snippet->title }}</h3>
-            <p>{{ $snippet->content }}</p>
 
-            <div class="tags">
-                <strong>Etiquetas:</strong>
-                @foreach ($snippet->tags as $tag)
-                    <span class="tag">{{ $tag->name }}</span>
-                @endforeach
-            </div>
-        </div>
-    @endforeach
+  @foreach ($snippets as $snippet)
+    <x-snippet-single :snippet=$snippet />
+  @endforeach
+
 @endif
 </x-layout>
