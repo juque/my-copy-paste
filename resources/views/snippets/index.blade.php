a48ff8f6a5ed94397d5a9e39731082ca2710ea92
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-10-23 12:35:30 -0300

    add some style touch

--- resources/views/snippets/index.blade.php
+++ resources/views/snippets/index.blade.php
@@ -2,11 +2,13 @@
 
   <x-slot:heading>Snippets</x-slot:heading>
 
-  <div class="my-10 py-3 text-center border rounded-xl bg-emerald-500 text-white"><a class="text-4xl" href="{{ route('snippets.create') }}">Nuevo</a></div>
+  <div class="my-10 py-3 text-center border rounded-xl bg-[#257180] text-white">
+    <a class="text-4xl" href="{{ route('snippets.create') }}">Nuevo</a>
+  </div>
 
   <div class="tags text-center">
     @foreach($tags as $tag)
-      <span class="inline-block border bg-blue-200 rounded-md p-1">
+      <span class="inline-block border bg-[#CB6040] text-white rounded-2xl p-2">
         <a href="{{ route('snippets.byTag', $tag->name) }}">{{ $tag->name }}</a>
       </span>
     @endforeach
